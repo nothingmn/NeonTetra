53286a03427e521b2acc640c20042a433a8caaac
Rob <rob.chartier@gmail.com>
2019-11-28 12:25:46 -0800

    refactored method to load app config from files

--- NeonTetra.DI/Containers/ConfigurationRegistrationContainer.cs
+++ NeonTetra.DI/Containers/ConfigurationRegistrationContainer.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using NeonTetra.Contracts;
 using NeonTetra.Contracts.Configuration;
 using NeonTetra.Contracts.Infrastructure;
 using NeonTetra.Core.Configuration;
 using System.Threading;
 using System.Threading.Tasks;
+using NeonTetra.Contracts.Logging;
 using NeonTetra.Contracts.Membership;
 using NeonTetra.Contracts.Serialization;
 
@@ -28,36 +31,53 @@ namespace NeonTetra.DI.Containers
         {
             var config = container.Resolve<IConfiguration>();
             var appData = config.AppDataDirectory;
+            var log = container.Resolve<ILogFactory>().CreateLog(this.GetType());
 
             if (!System.IO.Directory.Exists(appData.FullName)) System.IO.Directory.CreateDirectory(appData.FullName);
 
-            await LoadUsersAsync(container, config);
+            await LoadUsersAsync(log, container, config);
         }
 
-        private async Task LoadUsersAsync(IDIContainer container, IConfiguration configuration)
+        private async Task LoadUsersAsync(ILog log, IDIContainer container, IConfiguration configuration)
         {
             var accountManager = container.Resolve<IAccountManager>();
             var serializer = container.Resolve<IDeserialize>();
-            var usersConfigFolder = System.IO.Path.Combine(configuration.AppDataDirectory.FullName, "Users");
-
-            if (!System.IO.Directory.Exists(usersConfigFolder)) System.IO.Directory.CreateDirectory(usersConfigFolder);
-
-            foreach (var userConfig in System.IO.Directory.GetFiles(usersConfigFolder))
+            await LoadConfigForType<List<IUser>>(log, serializer, configuration, "Users", async users =>
             {
-                if (System.IO.File.Exists(userConfig))
+                if (users != default(List<IUser>))
                 {
-                    var userJson = System.IO.File.ReadAllText(userConfig);
-                    if (!string.IsNullOrEmpty(userJson))
+                    foreach (var user in users)
                     {
-                        var users = serializer.Deserialize<List<IUser>>(userJson);
-                        if (users != null && users.Count > 0)
+                        await accountManager.RegisterUser(user);
+                    }
+                }
+            });
+        }
+
+        private async Task LoadConfigForType<T>(ILog log, IDeserialize serializer, IConfiguration configuration, string appDataSubFolderName, Func<T, Task> foundItems)
+        {
+            if (foundItems == null) return;
+            var configFolder = System.IO.Path.Combine(configuration.AppDataDirectory.FullName, appDataSubFolderName);
+
+            if (!System.IO.Directory.Exists(configFolder)) System.IO.Directory.CreateDirectory(configFolder);
+
+            foreach (var file in System.IO.Directory.GetFiles(configFolder))
+            {
+                try
+                {
+                    if (System.IO.File.Exists(file))
+                    {
+                        var json = System.IO.File.ReadAllText(file);
+                        if (!string.IsNullOrEmpty(json))
                         {
-                            foreach (var user in users)
-                            {
-                                await accountManager.RegisterUser(user);
-                            }
+                            var items = serializer.Deserialize<T>(json);
+                            if (items != null) await foundItems(items);
                         }
                     }
+                }
+                catch (Exception e)
+                {
+                    log.Error(e, "Could not load config from file:{0}", file);
                 }
             }
         }
